e8c4b431361cc90f253d426a09dae180d1d1a887
Abic26 <abicsupa@gamil.com>
2023-02-26 18:31:57 -0600

    termine

--- ejer_js/ejercio5/index5.js
+++ ejer_js/ejercio5/index5.js
@@ -2,32 +2,29 @@ function procesar(){
 let juan = document.getElementById('juan').value
 let pedro = document.getElementById('pedro').value
 let maria = document.getElementById('maria').value  
+let mayor = juan
 
-
-if(pedro,maria<juan){
-    alert(`juan es mayor`)
-}else if(maria,juan<pedro){
-    alert(`pedro es mayor`)
-}else if(juan,pedro<maria){
-        alert(`maria es mayor`)
-    }
-
-//   let precio = document.getElementById('precio').value
-//   let descuento = parseInt(document.getElementById('descuento').value)
-//   alert(`su total a pagar es ${precio-(precio * descuento / 100)}`)
-//   const totalDinero = precio-(precio * descuento / 100)
-//   setLocalStorage(totalDinero)
+if (pedro > mayor) {
+    mayor = pedro;
+}
+if (maria > mayor) {
+    mayor = maria;
+}
+alert("La edad mayor es " + mayor);
+limpiador()
+setLocalStorage(mayor)
 }
 
-// arrayTotal = []
+arrayTotal = []
 
-// function setLocalStorage(precio){
-//   arrayTotal.push(precio)
-//   localStorage.setItem("arrayTotal",JSON.stringify(arrayTotal))
-//   alert(`El precio se guardo en la base de datos`)
-//   limpiador()
-// }
-// function limpiador(){
-//   document.getElementById('precio').value=0
-//  document.getElementById('descuento').value=0
-// }
+function setLocalStorage(mayor){
+  arrayTotal.push(mayor)
+  localStorage.setItem("arrayTotal",JSON.stringify(arrayTotal))
+  alert(`se guardo en la base de datos`)
+  limpiador()
+}
+function limpiador(){
+  document.getElementById('juan').value=0
+ document.getElementById('pedro').value=0
+ document.getElementById('maria').value=0
+}
